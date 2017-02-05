b36f2df24864859ff7d5a8307ae21878a9928a69
Lisa Corneau <DLMK831@gmail.com>
2017-02-05 13:31:16 -0500

    many changes to user input error messages

--- main.py
+++ main.py
@@ -15,6 +15,10 @@
 # limitations under the License.
 #
 import webapp2
+import re
+import cgi
+import os
+from string import letters
 page_header = """
 <!DOCTYPE html>
 
@@ -28,34 +32,31 @@ page_header = """
     </head>
     <body>
     <h1>Signup</h1>
-        <form method="post">
+        <form action="/add" method="post">
             <table>
                 <tr>
                     <td><label for="username">Username</label></td>
                     <td>
-                        <input name="username" type="text" value="" required>
-                        <span class="error"></span>
+                        <input name="username" type="text" >
+                        <span p class='error'></span>
                     </td>
                 </tr>
                 <tr>
                     <td><label for="password">Password</label></td>
                     <td>
-                        <input name="password" type="password" required>
-                        <span class="error"></span>
+                        <input name="password" type="password"/>
                     </td>
                 </tr>
                 <tr>
                     <td><label for="verify">Verify Password</label></td>
                     <td>
-                        <input name="verify" type="password" required>
-                        <span class="error"></span>
+                        <input name="verify" type="password"/>
                     </td>
                 </tr>
                 <tr>
                     <td><label for="email">Email (optional)</label></td>
                     <td>
-                        <input name="email" type="email" value="">
-                        <span class="error"></span>
+                        <input name="email" type="email"/>
                     </td>
                 </tr>
             </table>
@@ -64,16 +65,68 @@ page_header = """
     </body>
 </html>
 """
-
 page_footer = """
 </body>
 </html>
 """
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
-        content = page_header + "<p>" +  "</p>" + page_footer
+        error = self.request.get("error")
+        error_element = "<p class='error'>" + error + "</p>" if error else ""
+
+        main_content = error_element
+        content = page_header  + main_content   + page_footer
         self.response.write(content)
+
+
+
+class AddResponse(webapp2.RequestHandler):
+    def post(self):
+        error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+#        params = dict(username = username,
+#                      email = email)
+
+        if not valid_username(username):
+            error = "That's not a valid username."
+            error_escaped = cgi.escape(error, quote=True)
+            self.redirect('/?error=' + error_escaped)
+
+        if not valid_password(password):
+            error ="That wasn't a valid password."
+            error_escaped = cgi.escape(error,quote=True)
+            self.redirect('/?error=' + error_escaped)
+        elif password != verify:
+            error = "Your passwords didn't match."
+            error_escaped = cgi.escape(error, quote=True)
+            self.redirect('/?error=' + error_escaped)
+
+        if not valid_email(email):
+            error = "That's not a valid email."
+            error_escaped = cgi.escape(error, quote=True)
+            self.redirect('/?error=' + error_escaped)
+
+        if error:
+             error_escaped = cgi.escape(error, quote=True)
+        else:
+            self.redirect('/?username=' + username)
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/add', AddResponse)
 ], debug=True)
